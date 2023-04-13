3c848c5d06311a60a05264f4081dbeb2df3baf26
Pradosh Kumar Panda <110518909+pradosh058@users.noreply.github.com>
2023-04-13 20:23:46 +0530

    Create bubbleSort.java

---
+++ bubbleSort.java
@@ -0,0 +1,12 @@
+public static void bubbleSort(int arr[], int n)
+    {
+        for(int i=0;i<n-1;i++){
+            for(int j=0;j<n-i-1;j++){
+                if(arr[j]>arr[j+1]){
+                    int temp=arr[j+1];
+                    arr[j+1]=arr[j];
+                    arr[j]=temp;
+                }
+            }
+        }
+    }
